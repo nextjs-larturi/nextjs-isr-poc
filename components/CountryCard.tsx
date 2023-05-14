adf970fefcf4fdcd9c58e4a6aaa2b639882832cf
Leandro Arturi <lea.arturi@gmail.com>
2023-05-14 14:24:05 -0300

    Estilos

--- components/CountryCard.tsx
+++ components/CountryCard.tsx
@@ -7,8 +7,10 @@ interface Props {
 
 const CountryCard: React.FC<Props> = ({ country }) => {
   return (
-    <div className="bg-black rounded-lg shadow-lg p-4">
-      <h2 className="text-2xl font-bold text-neon-green">{country.name}</h2>
+    <div className="bg-black rounded-lg shadow-lg p-4 mb-3 w-1/5">
+      <h2 className="text-2xl font-bold text-blue-600">
+        {country.name.charAt(0).toUpperCase() + country.name.slice(1)}
+      </h2>
       <div className="text-white">
         <p>Población: {country.population}</p>
         <p>Continente: {country.continent}</p>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,32 @@
-import type { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import axios from 'axios'
-import styles from '../styles/Home.module.css'
 import { Country } from '../types/country'
-import { getCountryInfo } from '../utils/getCountryInfo'
 import CountryCard from '../components/CountryCard'
 
 interface Props {
-  countries: Country[]
+  countries: Country[];
+  apiUrl: string;
 }
 
-const Home: NextPage<Props> = ({ countries }) => {
+const Home: NextPage<Props> = ({ countries, apiUrl }) => {
   return (
-    <div className={styles.container}>
+    <div className='container mx-auto px-4 pt-8'>
       <Head>
         <title>Next ISR PoC</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
-        <h1>Next ISR PoC</h1>
-        <div>
+      <div className='flex flex-col justify-center'>
+        <h1 className='text-3xl'>Next ISR PoC</h1>
+        <hr />
+        <h2 className='text-xl mt-6'>List of countries</h2>
+        <p className='text-sm text-gray-500'>
+          <a href={`${apiUrl}/country`} target="_blank" rel="noopener noreferrer">
+            {`${apiUrl}/country`}
+          </a>
+        </p>
+        <div className='mt-3'>
           {
             countries.map(country => (
               <CountryCard key={country.id} country={country} />
@@ -37,7 +43,8 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 
   return {
     props: {
-      countries: data
+      countries: data,
+      apiUrl: process.env.API_URL
     },
     revalidate: 60 // 60 seconds
   }
